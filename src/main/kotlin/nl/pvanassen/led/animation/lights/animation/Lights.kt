56c4a75db587ad02896022cc50adf9fa03336b05
Paul van Assen <source@pvanassen.nl>
2022-12-24 11:06:53 +0100

    Migrated

---
+++ src/main/kotlin/nl/pvanassen/led/animation/lights/animation/Lights.kt
@@ -0,0 +1,67 @@
+package nl.pvanassen.led.animation.lights.animation
+
+import nl.pvanassen.led.animation.common.canvas.Canvas
+import nl.pvanassen.led.animation.common.model.Animation
+import nl.pvanassen.led.animation.common.util.ColorUtils
+import java.util.*
+import kotlin.collections.ArrayList
+
+
+class Lights(private val canvas: Canvas, private val pixels: List<Int>) : Animation<Any> {
+
+    private val random = Random()
+
+    private var cnt: Double = 0.toDouble()
+
+    private val positions: MutableList<List<Boolean>>
+
+    private var secondsSinceReset = 0.0
+
+    init {
+        positions = ArrayList()
+        positions.addAll(createPositions())
+    }
+
+    private fun createPositions(): List<List<Boolean>> {
+        return (pixels.indices).map {
+            val litPixels = arrayOf(random.nextInt(pixels[it]), random.nextInt(pixels[it]), random.nextInt(pixels[it]))
+            (0 until pixels[it]).map { pixel ->
+                litPixels.contains(pixel)
+            }
+        }
+    }
+
+    override fun getFrame(seed: Long, frame: Int, nsPerFrame: Int, helper: Any): ByteArray {
+        val color = this.color
+        secondsSinceReset += nsPerFrame / 1000_000_000.0
+
+        if (secondsSinceReset > 120) {
+            secondsSinceReset = 0.0
+            positions.clear()
+            positions.addAll(createPositions())
+        }
+
+        (pixels.indices).forEach { strip ->
+            (0 until pixels[strip]).map { pixel ->
+                val pixelColor = if (positions[strip][pixel]) {
+                    color
+                } else {
+                    0
+                }
+                canvas.setValue(strip, pixel, pixelColor)
+            }
+        }
+
+        return canvas.getValues()
+    }
+
+    private val color: Int
+        get() {
+            cnt += 0.01.coerceAtMost(Math.random() / 1000.0)
+            if (cnt > 1) {
+                cnt--
+            }
+            return ColorUtils.makeColorHSB(cnt.toFloat(), 1f, 1f)
+        }
+
+}
---
+++ src/main/kotlin/nl/pvanassen/led/animation/lights/animation/LightsFactory.kt
@@ -0,0 +1,13 @@
+package nl.pvanassen.led.animation.lights.animation
+
+import io.ktor.server.config.*
+import nl.pvanassen.led.animation.common.canvas.Canvas
+import nl.pvanassen.led.animation.common.model.Animation
+import nl.pvanassen.led.animation.common.model.AnimationFactory
+import nl.pvanassen.led.animation.common.model.Registration
+
+class LightsFactory: AnimationFactory<Any> {
+    override fun getAnimation(canvas: Canvas, pixels: List<Int>, config: ApplicationConfig) = Lights(canvas, pixels)
+
+    override fun getRegistrationInfo() = Registration("lights")
+}
