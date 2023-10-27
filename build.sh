10d993a1edde3b2569972e56dcbbc6e99368a8e8
Paul van Assen <source@pvanassen.nl>
2023-10-27 12:41:11 +0200

    1.7.0 Build

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-DOCKER_BUILDKIT=1 docker build . -t animation-sunrise
+docker buildx build . --platform linux/amd64,linux/arm64 -t pvanassen.nl/led/animation-sunrise:latest --push --progress=plain
