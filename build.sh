f9e66bf278a3b40ca35b1c617085216ffe5bbc76
Paul van Assen <source@pvanassen.nl>
2023-10-27 12:44:21 +0200

    1.7.0 Version update Build update

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-DOCKER_BUILDKIT=1 docker build . -t animation-sunset
+docker buildx build . --platform linux/amd64,linux/arm64 -t pvanassen.nl/led/animation-sunset:latest --push --progress=plain
