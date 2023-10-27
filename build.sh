826d5fb016ec5513bd77b12114aa2764a123e417
Paul van Assen <source@pvanassen.nl>
2023-10-27 13:14:12 +0200

    1.7.0 - Version update - Build update

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-DOCKER_BUILDKIT=1 docker build . -t animation-fireworks
+docker buildx build . --platform linux/amd64,linux/arm64 -t pvanassen.nl/led/animation-fireworks:latest --push --progress=plain
