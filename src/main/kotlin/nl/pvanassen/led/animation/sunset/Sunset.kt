9d086fca5db10d3748a0badb10901df5fde147d1
Paul van Assen <source@pvanassen.nl>
2023-11-10 10:05:10 +0100

    1.7.0 Cleanup

--- src/main/kotlin/nl/pvanassen/led/animation/sunset/Sunset.kt
+++ src/main/kotlin/nl/pvanassen/led/animation/sunset/Sunset.kt
@@ -4,34 +4,29 @@ import nl.pvanassen.led.animation.common.canvas.Canvas
 import nl.pvanassen.led.animation.common.model.Animation
 import java.awt.Image
 import java.awt.image.BufferedImage
-import java.io.IOException
-import java.io.UncheckedIOException
 import javax.imageio.ImageIO
 
-class Sunset(private val canvas: Canvas): Animation<Any> {
+class Sunset(private val canvas: Canvas) : Animation<Any> {
 
     private val sunset: BufferedImage
 
     private val waitFrames = 2
 
-    private val frames:Int
+    private val frames: Int
 
-    private var y:Int = 0
+    private var y: Int = 0
 
     init {
-        try {
-            val img = ImageIO.read(javaClass.getResourceAsStream("/sunset.png"))
-            sunset = if (img.width < canvas.getWidth()) {
-                val factor = canvas.getWidth() / img.width.toDouble()
-                val scaled = img.getScaledInstance((img.width * factor).toInt(), (img.height * factor).toInt(), Image.SCALE_SMOOTH)
-                val buffer = BufferedImage(scaled.getWidth(null), scaled.getHeight(null), BufferedImage.TYPE_INT_RGB)
-                buffer.graphics.drawImage(scaled, 0, 0, null)
-                buffer
-            } else {
-                img
-            }
-        } catch (e: IOException) {
-            throw UncheckedIOException(e)
+        val img = ImageIO.read(javaClass.getResourceAsStream("/sunset.png"))
+        sunset = if (img.width < canvas.getWidth()) {
+            val factor = canvas.getWidth() / img.width.toDouble()
+            val scaled =
+                img.getScaledInstance((img.width * factor).toInt(), (img.height * factor).toInt(), Image.SCALE_SMOOTH)
+            val buffer = BufferedImage(scaled.getWidth(null), scaled.getHeight(null), BufferedImage.TYPE_INT_RGB)
+            buffer.graphics.drawImage(scaled, 0, 0, null)
+            buffer
+        } else {
+            img
         }
         frames = sunset.height * waitFrames
 
